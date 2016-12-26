8b565652919b7fd1247de164bb5e55df3d45e841
cinguettare <cinguettare@163.com>
2016-12-27 07:18:44 +0800

    Update Myuseragent.py

--- Myuseragent.py
+++ Myuseragent.py
@@ -1,53 +1,91 @@
-import re
 import time
 import requests
-import random
+from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+class ua():
 
-class Myheader():
+    # 获取代理ip网址
+    def Get_url(self, page):
+        url = 'http://www.kuaidaili.com/free/inha/'
+        url_list = []
+        for p in range(page, page+5):
+            new_url = url + str(p)
+            url_list.append(new_url)
+        return url_list
 
-    def __init__(self):
-        self.iplist = []                                      # 存放ip
-        html = requests.get("http://haoip.cc/tiqu.htm")
-        iplistn = re.findall(r'r/>(.*?)<b', html.text, re.S)  # 获取ip
-        for ip in iplistn:
-            i = re.sub('\n', '', ip)                          # 替换\n
-            self.iplist.append(i.strip())
+    # 获得代理ip
+    def Get_ip(self, page):
+        try:
+            print(u"获取代理中。。。")
+            url_list = self.Get_url(page)
+            for u in url_list:
+                html = requests.get(u, headers=headers)
+                soup = BeautifulSoup(html.text, 'lxml')
+                iplist = soup.find('tbody').find_all('tr')
+                for i in iplist:
+                    iii = i.find_all('td', limit=2)
+                    ip = iii[0].string
+                    port = iii[1].string
+                    proxy = ip+':'+port
+                    self.Detect_ip(proxy)
+        except:
+            print(u"代理已用完！")
 
-    def get(self, url, timeout, proxy=None, num_retries=5):
-        ua = UserAgent().random                              
-        headers = {'User-Agent': ua}                          # 获得UA
+    # 检测ip是否能够使用
+    def Detect_ip(self, ip):
+        ip = ''.join(ip.strip())
+        proxy = {'http': ip}
+        test_time = 2
+        try:
+            requests.get(url_test, headers=headers, proxies=proxy, timeout=test_time)
+            print(u'%s合格， 保存' % proxy)
+            ip_list.append(ip)
+        except:
+            print(u'%s不合格' % proxy)
+            pass
 
-        if proxy == None:
-            try:                       # 先不使用代理
-                return requests.get(url, headers=headers, timeout=timeout)
-            except:                    # 爬虫被阻止后，进行这一步
-                if num_retries > 0:
-                    time.sleep(10)
-                    print(u'获取网页出错，10s后将获取倒数第', num_retries, u'次')
-                    return self.get(url, timeout, num_retries-1)
-                else:
-                    print(u"开始使用代理")
-                    time.sleep(10)
-                    IP = ''.join(str(random.choice(self.iplist)).strip())
-                    proxy = {'http':IP}
-                    return self.get(url, timeout, proxy)
-        else:                          # 代理
+    # 获取后续的代理网址页面
+    def Get_proxy(self, page):
+        if ip_list:
+            self.Get_ip(page+5)
+        else:
+            proxy = ip_list.pop()
+            return proxy
+
+    # 代理
+    def get(self, url, proxy_flog=False, n=1):
+        if not proxy_flog:
             try:
-                IP = ''.join(str(random.choice(self.iplist)).strip())
-                proxy = {'http': IP}
-                return requests.get(url, headers=headers, proxies=proxy, timeout=timeout)
+                img_html = requests.get(url, headers=headers, timeout=10)
+                time.sleep(5)
+                return img_html
             except:
-                if num_retries > 0:
-                    time.sleep(10)
-                    IP = ''.join(str(random.choice(self.iplist)).strip())
-                    proxy = {'http': IP}
-                    print(u'正在更换代理，10s后将重新获取倒数第', num_retries, u'次')
-                    print(u'当前代理是：', proxy)
-                    return self.get(url, timeout, proxy, num_retries-1)
+                print(u"开始使用代理！")
+                return self.get(url, proxy_flog=True)
+        else:
+            try:
+                time.sleep(5)
+                proxy = {'http': self.Get_proxy(n)}
+                img_html = requests.get(url, headers=headers, proxies=proxy, timeout=10)
+                print(u"现在使用的代理为：", proxy)
+                if img_html.status_code == 200:
+                    return img_html
                 else:
-                    print(u'代理出错！ 取消代理！')
-                    return self.get(url, 3)
+                    time.sleep(5)
+                    return self.get(url, proxy_flog=True)
+            except:
+                n += 5
+                return self.get(url, proxy_flog=True, n=n)
 
-myrequest = Myheader()
+
+
+
+
+
+
+ip_list = []
+url_test = 'https://www.baidu.com'
+headers = {'User-Agent': UserAgent().random}
+myrequest = ua()
+
